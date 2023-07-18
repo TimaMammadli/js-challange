18eed7c0361680eee2666b013b5aaf36a2662e54
Teymur <m.teymur2003@gmail.com>
2023-07-18 16:45:05 +0400

    Tima

---
+++ doc.js
@@ -0,0 +1,68 @@
+        //  1
+
+// let a = prompt("Hello World")
+
+// console.log(a);
+
+// let b = prompt("Do You Want Next ?")
+
+// console.log(b);
+
+// let num = prompt("Password")
+
+// console.log(num);
+
+         
+
+                    // 2
+// let a = parseInt(prompt("First number?", 1));
+// let b = parseInt(prompt("Second number?", 2));
+
+// alert(a + b); 3
+
+
+                    //  3
+// let number;
+
+// do {
+//   number = parseInt(prompt("Enter a number greater than 100", ""));
+// } while (number <= 100 || isNaN(number));
+
+                  //  4
+// let number = prompt(4, 0);
+
+// if (number > 0) {
+//   alert(1);
+// } else if (number < 0) {
+//   alert(-1);
+// } else {
+//   alert(0);
+// }
+
+// let message = (login == 'Employee') ? 'Hello' :
+//               (login == 'Director') ? 'Greetings' :
+//               (login == '') ? 'No login' :
+//               'Something else';
+
+
+
+                              //  5
+                              
+// let login = prompt("Enter the login please");
+
+// login = (login === "director") ? "Hello" :
+// (login === "null") ? "Greetings" :
+//  (login === "employee") ? "no login" :
+//  "?";
+                              
+// alert(login);
+                                              
+ 
+// let number1 = parseInt(prompt ("Enter the first number"));
+// let number2 = parseInt(prompt ("Enter the pow off number"));
+// function powOfNumber(x,y)
+// {
+//   return x**y; 
+// }
+// console.log(powOfNumber(number1,number2));
+
